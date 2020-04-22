60d8cf8756cd98e66efe27d41a1bb40feace1bab
Edmond <edmond.b@hotmail.co.uk>
2020-04-22 16:17:41 +0100

    Added charging for journey, stores enrty and exit station and saves journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,34 +1,47 @@
 class Oystercard
 
-MIN_BALANCE = 1
-MAX_BALANCE = 90
-attr_reader :balance
+  MIN_CHARGE = 1
+  MAX_BALANCE = 90
+  attr_reader :balance, :entry_station, :history
+  attr_accessor :entry_station, :exit_station
 
-def initialize
-  @balance = 0
-  @in_use = false
-end
+  def initialize
+    @balance = 0
+    @entry_station
+    @exit_station
+    @history = {}
+  end
 
-def top_up(amount)
-  fail "Max balance reached: #{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
-  @balance += amount
-end
+  def top_up(amount)
+    fail "Max balance reached: #{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
+    @balance += amount
+  end
 
-def deduct(amount)
-  @balance -= amount
-end
+  def touch_in(station)
+    fail "Insufficient funds" if @balance < MIN_CHARGE
+    @entry_station = station
+  end
 
-def touch_in
-  fail "Insufficient funds" if @balance < MIN_BALANCE
-  @in_use = true
-end
+  def in_journey?
+    !!entry_station
+  end
 
-def in_journey?
-  @in_use
-end
+  def touch_out(station)
+    deduct
+    @entry_station = nil
+    @exit_station = station
+    store_journey
+    @exit_station = nil
+  end
 
-def touch_out
-  @in_use = false
-end
+  def store_journey
+    @history[entry_station] = exit_station
+  end
+
+  private
+
+  def deduct(amount=MIN_CHARGE)
+    @balance -= amount
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  it 'has an empty hash' do
+    expect(subject.history).to be_empty
+  end
+
+  let(:station){ double :station }
+  it 'remembers the entry station' do
+    subject.top_up(10)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station
+  end
+
   it 'Has a initial balance of 0' do
     expect(subject.balance).to eq 0
   end
@@ -19,11 +31,7 @@ describe Oystercard do
     before(:all) do
       subject.top_up(Oystercard::MAX_BALANCE)
 
-      it 'lets you deduct from card' do
-        expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
-      end
-
-      describe 'lets you touch in' do
+     it 'lets you touch in' do
         specify { expect(subject.touch_in).to be_truthy }
       end
     end
@@ -31,10 +39,17 @@ describe Oystercard do
 
   context 'it has low or no balance' do
     before(:all) do
-      subject.top_up(Oystercard::MIN_BALANCE)
+      subject.top_up(Oystercard::MIN_CHARGE)
+
+      it 'store the journey' do
+        subject.touch_in('entry')
+        subject.touch_out('exit')
+        expect(history).to have_key(:entry)
+        expect(history).to have_value(:exit)
+      end
 
       it 'throws an error if isufficient funds' do
-        min_balance = Oystercard::MIN_BALANCE
+        min_balance = Oystercard::MIN_CHARGE
         expect{ subject.touch_in }.to raise_error("Insufficient funds")
       end
     end
@@ -45,7 +60,11 @@ describe Oystercard do
   end
 
   describe 'lets you touch out' do
-    specify { expect(subject.touch_out).not_to be_truthy }
+    specify { expect(subject.touch_out(station)).not_to be_truthy }
+  end
+
+  it 'deducts money when you touch out' do
+    expect { subject.touch_out(station) }.to change{ subject.balance }.by -Oystercard::MIN_CHARGE
   end
 
 end
